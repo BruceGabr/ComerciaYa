280ad7eeee2b8a93f7b6e755de934412642a4e0f
Bruce <bruce.gabr@gmail.com>
2025-05-04 00:36:23 -0500

    Home page modificado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css"; // Crearemos este archivo para estilos
 import artesaniasImg from "../assets/images/artesanias.webp";
 import comidaCaseraImg from "../assets/images/comida-casera.jpg";
 import productosNaturalesImg from "../assets/images/productos-naturales.webp";
+import bannerImg from "../assets/images/contabilidad.png";
 
 const Home = () => {
   return (
     <div className="home">
       {/* Sección de Bienvenida */}
       <section className="home__banner">
-        <h2 className="home__title">Bienvenidos a EmprendeLocal</h2>
-        <p className="home__description">
-          Conoce los mejores productos y servicios de emprendedores locales.
-        </p>
-        <button className="home__cta">Explorar Productos</button>
+        <div className="container home__banner-content">
+          <div className="home__banner-image">
+            <img src={bannerImg} alt="Contabilidad" />
+          </div>
+          <div className="home__banner-text">
+            <h2 className="home__title">
+              Explora la Creatividad de tu Comunidad
+            </h2>
+            <p className="home__description">
+              Conoce los mejores productos y servicios de emprendedores locales.
+            </p>
+            <p className="home__description">
+              Descubre variedad, calidad y autenticidad en cada propuesta, y
+              apoya el crecimiento de tu comunidad con cada elección.
+            </p>
+            <Link to="/explorar" className="home__cta">
+              Explorar Productos
+            </Link>
+          </div>
+        </div>
       </section>
 
       {/* Sección de productos destacados */}
